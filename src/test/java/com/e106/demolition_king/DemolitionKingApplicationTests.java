f208d4afe5f95ad8d168e9cb3274693a9baf1890
홍지훈 <wlgns9807@naver.com>
2025-07-28 16:19:43 +0900

    feat: yml_update2

--- src/test/java/com/e106/demolition_king/DemolitionKingApplicationTests.java
+++ src/test/java/com/e106/demolition_king/DemolitionKingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemolitionKingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
