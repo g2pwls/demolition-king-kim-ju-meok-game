c024e3e2a2734d0f39f6a8a859e0780247aae595
홍지훈 <wlgns9807@naver.com>
2025-08-05 12:35:12 +0900

    deletefolder

--- Back/src/main/java/com/e106/demolition_king/constructure/vo/in/ConstructureSaveRequestVo.java
+++ Back/src/main/java/com/e106/demolition_king/constructure/vo/in/ConstructureSaveRequestVo.java
@@ -1,5 +1,6 @@
 package com.e106.demolition_king.constructure.vo.in;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class ConstructureSaveRequestVo {
+    @Schema(description = "토큰에 가져와 넣을값이라 빈값으로 두기", example = "")
     private final String userUuid;
     private final List<Integer> constructureSeqList;
 }
--- Back/src/main/java/com/e106/demolition_king/skin/vo/in/SelectSkinRequestVo.java
+++ Back/src/main/java/com/e106/demolition_king/skin/vo/in/SelectSkinRequestVo.java
@@ -1,5 +1,6 @@
 package com.e106.demolition_king.skin.vo.in;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,8 @@ import java.util.List;
 @Setter
 @Builder
 public class SelectSkinRequestVo {
+    @Schema(description = "토큰에 가져와 넣을값이라 빈값으로 두기", example = "")
     private String userUuid;
+    @Schema(description = "스킨내부코드", example = "")
     private Integer playerSkinItemSeq;
 }
